09abe7bb647a24067c7ff52e2460d23fb0a1c35d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "private_dns_resolver_inbound_endpoints_id" {
+  description = "Map of id values across all private_dns_resolver_inbound_endpoints, keyed the same as var.private_dns_resolver_inbound_endpoints"
+  value       = { for k, v in azurerm_private_dns_resolver_inbound_endpoint.private_dns_resolver_inbound_endpoints : k => v.id }
+}
 output "private_dns_resolver_inbound_endpoints_ip_configurations" {
   description = "Map of ip_configurations values across all private_dns_resolver_inbound_endpoints, keyed the same as var.private_dns_resolver_inbound_endpoints"
   value       = { for k, v in azurerm_private_dns_resolver_inbound_endpoint.private_dns_resolver_inbound_endpoints : k => v.ip_configurations }
